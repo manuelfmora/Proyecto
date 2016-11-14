3550a885ff2ea6b5315461b2f6121a417ec22b32
mfmor <mfmor@PC-TRABAJO>
2016-11-15 00:45:00 +0100

    Inserción de alumno completada y empezando con modificación y borrado

--- application/controllers/AlumnoInsert.php
+++ application/controllers/AlumnoInsert.php
@@ -1,31 +1,32 @@
 <?php
+
 /**
  * Description of login
  *
  * @author Manuel Mora
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
-class AlumnoInsert extends CI_Controller{
-    
-    
+
+class AlumnoInsert extends CI_Controller {
+
     public function __construct() {
         parent::__construct();
-        
+
         $this->load->helper('Formulario');
         $this->load->model('M_Provincias');
-        $this->load->model('M_Alumno');    
-        $this->load->library('form_validation');        
+        $this->load->model('M_Alumno');
+        $this->load->library('form_validation');
     }
 
     /**
      * Muestra el formulario de registro
      */
     public function index() {
-        
+
         $provincias = $this->M_Provincias->getProvincias();
-        
+
         $select = CreaSelect($provincias, 'cod_provincia');
-        
+
         $cuerpo = $this->load->view('V_AlumnoInsert', array('select' => $select), true);
 
         $this->load->view('V_Plantilla', Array(
@@ -38,7 +39,7 @@ class AlumnoInsert extends CI_Controller{
      * Comprueba que los datos introducidos en el formulario sean correctos
      */
     public function Alumno() {
-        
+
         $provincias = $this->M_Provincias->getProvincias();
         $select = CreaSelect($provincias, 'cod_provincia');
 
@@ -46,69 +47,41 @@ class AlumnoInsert extends CI_Controller{
 
         //Establecemos los mensajes de errores
         $this->setMensajesErrores();
-        
+
         //Establecemos reglas de validación para el formulario
         $this->setReglasValidacion();
-//|| !claves_check($this->input->post('clave'), $this->input->post('rep_clave'))
-        if ($this->form_validation->run() == FALSE ) {//Validación de datos incorrecta
-//            $errorclave = '';
-//
-//            if (!claves_check($this->input->post('clave'), $this->input->post('rep_clave'))) {//Si las claves no son iguales, se muestra error
-//                $errorclave = '<div class="alert alert-danger"><b>¡Error! </b> Las contraseñas no son iguales</div>';
-//            }
-//          
-          
+
+        if ($this->form_validation->run() == FALSE) {//Validación de datos incorrecta
+        
             $cuerpo = $this->load->view('V_AlumnoInsert', array(
-                                             'select' => $select), true);
+                'select' => $select), true);
 
             $this->load->view('V_Plantilla', Array(
-                                    'cuerpo' => $cuerpo,
-                                    'homeactive' => 'active'));
-            
-            
+                'cuerpo' => $cuerpo,
+                'homeactive' => 'active'));
         } else {//Validación de datos correcta
-            
+            print_r('Validación correcta');
             //Crea el array de los datos a insertar en la tabla usuario
             foreach ($this->input->post() as $key => $value) {
-//                if($key == 'clave')
-//                {
-//                    $data[$key] = password_hash($value, PASSWORD_DEFAULT);
-//                }
-//                else if ($key != 'rep_clave' && $key != 'GuardarUsuario')
-//                    $data[$key] = $value;
-                
+                if ($key == 'fechaNacimiento') {
+                    $fecha = $this->formato_mysql($value);
+                    $data[$key] =$fecha;
+                } else if( $key != 'GuardarUsuario'){
+                    $data[$key] = $value;
+                }
             }
             
             //Inserta en la tabla alumnado
             $this->M_Alumno->addalumno($data);
-            
-            redirect('Login/Login/'.$data['nombre_usu'], 'location', 301);
+
+            redirect('Login/Login/' . $data['nombre_usu'], 'location', 301);
         }
     }
-
-    /**
-     * Comprueba si un DNI es correcto
-     * @param String $dni DNI a comprobar
-     * @return boolean
-     */
-    function dni_check($dni) {
-
-        $numerosDNI = substr($dni, 0, 8);
-        $letraDNI = substr($dni, 8, 1);
-        $letraDNI = strtoupper($letraDNI);
-
-        if (is_numeric($numerosDNI) && ($letraDNI == dni_LetraNIF($dni))) {
-            return TRUE;
-        } else {
-            return FALSE;
-        }
-    }
-
     /**
      * Comprueba si un nombre de usuario está ya usado
      * @param String $nombre_usu Nombre del usuario a comprobar
      * @return boolean
-     */
+    */
     function nombreUsuRepetido_check($nombre_usu) {
 
         $countNomUsuario = $this->M_Alumno->getCount_NombreUsuario($nombre_usu);
@@ -119,35 +92,49 @@ class AlumnoInsert extends CI_Controller{
             return FALSE;
         }
     }
-      /**
+
+    /**
      * Comprueba si un NIE de alumno está ya usado
      * @param String $nie Nie del alumno a comprobar
      * @return boolean
      */
-    function numeroNieRepetido_chek($nie){
-        $cunetaNie=$this->M_Alumno->getCount_Nie($nie);
-        if($cunetaNie==0){
+    function numeroNieRepetido_chek($nie) {
+        $cunetaNie = $this->M_Alumno->getCount_Nie($nie);
+        if ($cunetaNie == 0) {
             return TRUE;
-        }else{
+        } else {
             return FALSE;
         }
-        
     }
-    function validar_fecha($str) {
-        
-        $patron = "/^(\d{1,2})-(\d{1,2})-(\d{4})$/";
-        if (preg_match($patron, $str)) {
-            return TRUE;
-        } else {
-            
-            return FALSE;
+
+    function formato_americano($date) {
+
+        if (!empty($date)) {
+            $var = explode('/', str_replace('-', '/', $date));
+            return "$var[2]/$var[1]/$var[0]";
         }
-    }function validar_telefono($tel){
-        $patron=" /^((\+?34([ \t|\-])?)?[9|6|7]((\d{1}([ \t|\-])?[0-9]{3})|(\d{2}([ \t|\-])?[0-9]{2}))([ \t|\-])?[0-9]{2}([ \t|\-])?[0-9]{2})$/ ";
-          if (preg_match($patron, $tel)) {
+    }
+
+    function formato_mysql($date) {
+
+        if (!empty($date)) {
+            $var = explode('/', str_replace('-', '/', $date));
+            return "$var[2]-$var[1]-$var[0]";
+        }
+    }
+
+    function validar_fecha($str) {
+        $date = $this->formato_americano($str);
+
+        return (!preg_match('/^(19|20)[0-9]{2}\/(0[1-9]|1[012])\/(0[1-9]|[12][0-9]|3[01])$/', $date)) ? FALSE : TRUE;
+    }
+
+    function validar_telefono($tel) {
+        $patron = " /^((\+?34([ \t|\-])?)?[9|6|7]((\d{1}([ \t|\-])?[0-9]{3})|(\d{2}([ \t|\-])?[0-9]{2}))([ \t|\-])?[0-9]{2}([ \t|\-])?[0-9]{2})$/ ";
+        if (preg_match($patron, $tel)) {
             return TRUE;
         } else {
-            
+
             return FALSE;
         }
     }
@@ -155,43 +142,43 @@ class AlumnoInsert extends CI_Controller{
     /**
      * Establece los mensajes de error que se mostrarán si no se valida correctamente el formulario
      */
-    function setMensajesErrores(){
+    function setMensajesErrores() {
         $this->form_validation->set_message('required', 'El campo %s está vacío');
         $this->form_validation->set_message('valid_email', 'Formato de correo electrónico incorrecto');
         $this->form_validation->set_message('integer', 'El campo %s debe ser un número de 5 dígitos');
         $this->form_validation->set_message('exact_length', 'El campo %s debe tener %s caracteres');
         $this->form_validation->set_message('integer', 'El campo %s debe ser númerico');
-        $this->form_validation->set_message('dni_check', 'Formato de DNI incorrecto');
-        $this->form_validation->set_message('numeroNieRepetido_chek', 'El campo %s ya existe');       
+//        $this->form_validation->set_message('dni_check', 'Formato de DNI incorrecto');
+        $this->form_validation->set_message('numeroNieRepetido_chek', 'El campo %s ya existe');
         $this->form_validation->set_message('validar_fecha', 'formato de fecha no v&aacute;lido');
         $this->form_validation->set_message('validar_telefono', 'El campo %s debe ser númerico');
     }
-   
+
     /**
      * Establece las reglas que deben seguir cada campo del formulario
      */
-    function setReglasValidacion(){
+    function setReglasValidacion() {
         $this->form_validation->set_rules('apellidos', 'apellidos', 'required');
         $this->form_validation->set_rules('nombre', 'nombre', 'required');
-        $this->form_validation->set_rules('dni', 'DNI', 'required|exact_length[9]|callback_dni_check');
+//        $this->form_validation->set_rules('dni', 'DNI', 'required|exact_length[9]|callback_dni_check');
         $this->form_validation->set_rules('nie', 'NIE', 'required|exact_length[5]|callback_numeroNieRepetido_chek');
         $this->form_validation->set_rules('fechaNacimiento', 'fecha nacimiento', 'required|callback_validar_fecha');
         $this->form_validation->set_rules('nombreT1', 'nombre titular 1', 'required');
-        $this->form_validation->set_rules('dniT1', 'DNI T1', 'required|exact_length[9]|callback_dni_check');
+//        $this->form_validation->set_rules('dniT1', 'DNI T1', 'required|exact_length[9]|callback_dni_check');
         $this->form_validation->set_rules('nombreT2', 'nombre titular 2', 'required');
-        $this->form_validation->set_rules('dniT2', 'DNI T2', 'required|exact_length[9]|callback_dni_check');
+//        $this->form_validation->set_rules('dniT2', 'DNI T2', 'required|exact_length[9]|callback_dni_check');
         $this->form_validation->set_rules('direccion', 'dirección', 'required');
         $this->form_validation->set_rules('cp', 'CP', 'required|integer|exact_length[5]');
         $this->form_validation->set_rules('poblacion', 'Población', 'required');
         $this->form_validation->set_rules('cod_provincia', 'provincia', 'required');
         $this->form_validation->set_rules('telefono1', 'Teléfono 1', 'required|exact_length[9]|callback_validar_telefono');
-        $this->form_validation->set_rules('telefono2', 'Teléfono 2', 'required|exact_length[9]|callback_validar_telefono');
+        $this->form_validation->set_rules('telefono2', 'Teléfono 2', 'exact_length[9]|callback_validar_telefono');
         $this->form_validation->set_rules('cod_provincia', 'provincia', 'required');
         $this->form_validation->set_rules('tipo', 'Tipo', 'required');
         $this->form_validation->set_rules('situacion', 'Situación', 'required');
         $this->form_validation->set_rules('implicacion_escolar', 'Implicación Escolar', 'required');
         //Preguntar si el correo es necesario.
 //        $this->form_validation->set_rules('correo', 'correo electrónico', 'required|valid_email');
-  
     }
+
 }
---
+++ application/views/V_AlumnoModify.php
@@ -0,0 +1,148 @@
+ <!-- Start Insert students -->
+  <section id="mu-reservation">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="mu-reservation-area">
+            <div class="mu-title">
+              <span class="mu-subtitle">Insertar un</span>
+              <h2>Alumno/a</h2>
+              <i class="fa fa-spoon"></i>              
+              <span class="mu-title-bar"></span>
+            </div>
+            <div class="mu-reservation-content">
+              <form class="mu-reservation-form" action="<?= base_url() . 'index.php/AlumnoInsert/Alumno' ?>" method="post">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <input type="text" class="form-control" value="<?= $datos('apellidos') ?>" placeholder="Apellidos" name="apellidos" maxlength="60">
+                        <?= form_error('apellidos'); ?>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <input type="text" class="form-control" value="<?= $datos('nombre') ?>" placeholder="Nombre" name="nombre" maxlength="40">
+                        <?= form_error('nombre'); ?>
+                    </div>
+                  </div> 
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <input type="text" class="form-control" value="<?= $datos('nie') ?>" placeholder="NIE" name="nie" class="input-text" maxlength="7">
+                        <?= form_error('nie'); ?>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <input type="text" class="form-control" value="<?= $datos('fechaNacimiento') ?>" placeholder="Fecha Nacimiento dd/mm/aaaa " name="fechaNacimiento">
+                      <?= form_error('fechaNacimiento'); ?>
+                    </div>
+                  </div>
+
+                   <!-- Edad Campo Calculado--->
+                   
+                   <!-- Foto Alumno --->
+                 <div class="col-md-6">
+                    <div class="form-group">
+                        <input type="text" class="form-control" value="<?= $datos('datos_medicos') ?>" placeholder="Datos Médicos" name="datos_medicos" >
+                        
+                    </div>
+                  </div>
+                 <div class="col-md-6">
+                    <div class="form-group">
+                        <input type="text" class="form-control" value="<?= $datos('datos_psicologicos') ?>" placeholder="Datos Psicológicos" name="datos_psicologicos" >
+                       
+                    </div>
+                  </div>          
+                 <div class="col-md-12">
+                    <div class="form-group">
+                        <input type="text" class="form-control" value="<?= $datos('informe_medico') ?>" placeholder="Informe Médico" name="informe_medico" >
+                        
+                    </div>
+                  </div>
+                     
+                  <div class="col-md-6">                      
+                    <div class="form-group">                        
+                      <input type="text" class="form-control" value="<?= $datos('nombreT1') ?>"placeholder="Tutor 1" name="nombreT1">
+                      <?= form_error('nombreT1'); ?>
+                   </div>
+                  </div>    
+                  <div class="col-md-6">
+                    <div class="form-group">                        
+                      <input type="text" class="form-control" value="<?= $datos('nombreT2') ?>"placeholder="Tutor 2" name="nombreT2">
+                      
+                   </div>
+                  </div>
+                  
+                  <!--Empieza a fallar--->
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <input type="text" class="form-control" value="<?= $datos('direccion') ?>" placeholder="Dirección" name="direccion" maxlength="40">
+                        <?= form_error('direccion'); ?>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <input type="text" class="form-control" value="<?= $datos('cp') ?>" placeholder="Codigo Postal" name="cp">
+                        <?= form_error('cp'); ?>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <input type="text" class="form-control" value="<?= $datos('poblacion') ?>" placeholder="Población" name="poblacion" >
+                        <?= form_error('poblacion'); ?>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                    <?= $select ?> 
+                    <?= form_error('cod_provincia'); ?>
+                    </div>
+                  </div>
+                    <div class="col-md-6">
+                    <div class="form-group">
+                        <input type="text" class="form-control" value="<?= $datos('telefono1') ?>" placeholder="Teléfono 1" name="telefono1" maxlength="9">
+                        <?= form_error('telefono1'); ?>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <input type="text" class="form-control" value="<?= $datos('telefono2') ?>" placeholder="Teléfono 2" name="telefono2" maxlength="9">
+                        <?= form_error('telefono2'); ?>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <input type="text" class="form-control" value="<?= $datos('tipo') ?>" placeholder="Tipo Familia" name="tipo" >
+                        <?= form_error('tipo'); ?>
+                    </div>
+                  </div> 
+                    <div class="col-md-6">
+                    <div class="form-group">
+                        <input type="text" class="form-control" value="<?= $datos('situacion') ?>" placeholder="Situación" name="situacion" >
+                        <?= form_error('situacion'); ?>
+                    </div>
+                  </div> 
+                    <div class="col-md-12">
+                    <div class="form-group">
+                        <input type="text" class="form-control" value="<?= $datos('implicacion_escolar') ?>" placeholder="Implicación Escolar" name="implicacion_escolar">
+                        <?= form_error('implicacion_escolar'); ?>
+                    </div>
+                  </div>
+                  <center>
+                    <div class="col-md-12">
+                      <div class="form-group">
+
+                        <button type="submit" name="GuardarUsuario" class="mu-readmore-btn">Registrarse</button>
+
+                      </div>
+                    </div>
+                  </center>
+                </div>
+              </form>      
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>  
+  <!-- End Insert students -->
