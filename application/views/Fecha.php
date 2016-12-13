24a4f00d6f4a3fd1b1034596ef4ac9030c6a49e9
Soporte <Soporte@DESSMAN-5>
2016-12-13 14:49:25 +0100

    Prueba disparador añadir fecha

--- application/views/Fecha.php
+++ application/views/Fecha.php
@@ -11,3 +11,15 @@ CREATE TRIGGER `deporientacion`.`tr_creafecha` BEFORE INSERT ON `deporientacion`
         INSERT INTO usuario (fecha_Inser) VALUES (1970-12-28)      
     END $$
 DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER 'deporientacion45' BEFORE INSERT ON 'alumno'
+    FOR EACH ROW BEGIN
+        INSERT INTO alumno (fecha_Inser) VALUES ('1970-12-28')      
+    END $$
+DELIMITER ;
+
+CREATE TRIGGER before_employee_update AFTER UPDATE ON alumno FOR EACH ROW BEGIN INSERT INTO alumno SET action = 'update', datos_medicos = "asd"; END
+
+
+DROP TRIGGER IF EXISTS `before_employee_update`;CREATE DEFINER=`root`@`localhost` TRIGGER `before_employee_update` BEFORE INSERT ON `alumno` FOR EACH ROW INSERT INTO alumno (nombre, apellidos, Usuario_idUsuario) VALUES ("2","2","1")
